46cd59be8b75f83c97a79fdfc5fa5c1d6a0029cf
Tom Cassar <cassar.thomas.e@gmail.com>
2022-01-26 17:18:03 +0000

    written interfaces for crypto key handling; done with openssl, parsed by me

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -67,3 +67,24 @@ class TestHash(TestCase):
         """Checks that hasher objects when not bytes are passed into it"""
         with self.assertRaises(hashes.HasherError):
             hasher = hashes.Hasher('abd')
+
+
+class TestRSAKeyLoading(TestCase):
+    """Tests for RSA Keys"""
+
+    def test_file_loading(self):
+        """Tests that file is being loaded correctly"""
+
+        expected_start = """RSA Private-Key: (2048 bit, 2 primes)
+modulus:
+    "00:ab:3c:ec:18:ae:8d:f6:8c:50:2e:d7:1c:e3:37:
+    "6c:ca:00:c5:e8:2a:ee:bf:9a:a5:04:79:4e:d2:b4:
+    "b3:40:09:3e:38:1c:be:2d:c8:4c:27:4d:a6:40:a2:
+    "d2:c3:79:a8:c4:78:68:81:39:49:a4:a7:9c:4e:eb:
+    "01:4b:f3:c3:fd:53:58:cf:68:23:04:b7:b8:0d:b6:
+    "b8:8c:fc:93:e7:d0:21:e3:46:c0:1c:8d:73:e4:2f:
+    "cf:a8:7f:36:6f:9d:53:5c:ef:b8:46:a0:bb:32:9a:
+    "c2:75:27:9e:35:74:1c:2f:95:7e:d4:e6:f7:d0:62:"""
+        # test assumes if it starts off fine it will continue being fine
+
+
