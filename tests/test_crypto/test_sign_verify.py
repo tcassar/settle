d472aab7f5e1f25ef60089e8b06129c53b809dcd
Tom Cassar <cassar.thomas.e@gmail.com>
2022-02-02 14:53:53 +0000

    interfaces for sign/verify test

---
+++ tests/test_crypto/test_sign_verify.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+"""
+Testing sign / verify through RSA working as expected
+"""
+
+from unittest import TestCase
+
+
+class TestRSA(TestCase):
+    """Just tests RSA parts"""
+    def test_encrypt(self):
+        """RSA Encryption test"""
+
+    def test_decrypt(self):
+        """RSA Decryption"""
+
+
+class TestSigning(TestCase):
+    """Tests entire sign / verify process"""
+    def test_sign(self):
+        pass
+
+    def test_verify(self):
+        pass
