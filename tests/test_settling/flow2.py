1df9e514dc96cfe51e86a8501f8808fdf0a53752
Tom Cassar <cassar.thomas.e@gmail.com>
2022-03-04 19:17:54 +0000

    adds flow across edge successfully

--- tests/test_settling/flow2.py
+++ tests/test_settling/flow2.py
@@ -8,13 +8,28 @@ from src.simplify.graph_objects import Vertex
 class TestFlowEdge(TestCase):
     """Tests for FlowEdge"""
 
+    def setUp(self) -> None:
+        self.edges = [FlowEdge(Vertex(0), 5 * n) for n in range(2)]
+        self.edges[0].flow = -3  # => unused capacity should be three
+
     def test_unused_capacity(self):
-        # builds two edges, residual and non-residual
-        # non-residual has capacity 5
-        edges = [FlowEdge(Vertex(0), 5*n) for n in range(2)]
-        edges[0].flow = -3  # => unused capacity should be three
-        for edge, cap in zip(edges, [3, 5]):
+        """builds two edges, residual and non-residual
+        non-residual has capacity 5 """
+
+        for edge, cap in zip(self.edges, [3, 5]):
             with self.subTest(edge):
                 self.assertEqual(edge.unused_capacity(), cap)
 
-        
+    def test_push_flow(self):
+        """Checks that we update flow by required amount"""
+        capacity0 = {edge: edge.unused_capacity() for edge in self.edges}
+        # push three units of capacity through each edge
+        for edge, exp in zip(self.edges, [0, 2]):
+            with self.subTest(edge):
+                edge.push_flow(3)
+                self.assertEqual(exp, edge.unused_capacity())
+
+        # try to push excess amount of flow down an edge
+        with self.subTest('exceed capacity'), self.assertRaises(FlowEdgeError):
+            self.edges[1].push_flow(2)
+
