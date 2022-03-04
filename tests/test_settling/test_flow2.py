bbe4c5e58e0e556f71f2efdf4b4952bfa7ce4eb8
Tom Cassar <cassar.thomas.e@gmail.com>
2022-03-04 23:17:06 +0000

    finds aug paths, augments flow

--- tests/test_settling/test_flow2.py
+++ tests/test_settling/test_flow2.py
@@ -35,7 +35,6 @@ class TestFlowEdge(TestCase):
 
 
 class TestFlowGraph(TestCase):
-
     def setUp(self) -> None:
         # make some nodes for a graph
         self.nodes = [Vertex(n, label=chr(n + 97)) for n in range(5)]
@@ -45,10 +44,10 @@ class TestFlowGraph(TestCase):
     def test_is_edge(self):
         a, b, c, d, e = self.nodes
 
-        with self.subTest('no edge'):
+        with self.subTest("no edge"):
             self.assertFalse(self.graph.is_edge(a, b))
 
-        with self.subTest('edge'):
+        with self.subTest("edge"):
             self.graph.add_edge(a, b, 4)
             self.assertTrue(self.graph.is_edge(a, b))
 
@@ -60,13 +59,13 @@ class TestFlowGraph(TestCase):
     def test_add_edge(self):
         a, b, c, d, e = self.nodes
         # check there aren't edges
-        with self.subTest('negative test'):
+        with self.subTest("negative test"):
             self.assertFalse(self.graph.is_edge(a, b))
             self.assertFalse(self.graph.is_edge(b, a, residual=True))
         self.graph.to_dot()
         self.graph.add_edge(a, b, 5)
         self.graph.to_dot()
-        with self.subTest('added'):
+        with self.subTest("added"):
             self.assertTrue(self.graph.is_edge(a, b))
             self.assertTrue(self.graph.is_edge(b, a, residual=True))
             self.assertFalse(self.graph.is_edge(b, a))
@@ -76,34 +75,77 @@ class TestFlowGraph(TestCase):
 
         self.graph.add_edge(a, b, 5)
 
-        with self.subTest('negative'):
+        with self.subTest("negative"):
             self.assertTrue(self.graph.is_edge(a, b))
             self.assertTrue(self.graph.is_edge(b, a, residual=True))
 
         self.graph.pop_edge(a, b)
 
-        with self.subTest('removed edge'):
+        with self.subTest("removed edge"):
             self.assertFalse(self.graph.is_edge(a, b))
-        with self.subTest('removed residual'):
+        with self.subTest("removed residual"):
             self.assertFalse(self.graph.is_edge(b, a, residual=True))
+
+    def test_flow_neighbours(self):
+        """Checks we get edges that have unused capacity, including residual"""
+        graph = self.graph
+        a, b, c, d, *_ = graph.nodes()
+        graph.add_edge(a, b, 10)
+        graph.add_edge(b, c, 2)
+        graph.add_edge(c, d, 10)
+        graph.add_edge(d, a, 10)
+
+        # TODO: Augment flow
+
+        graph.to_dot()
 
 
 class TestSimplify(TestCase):
-
-    def test_bottleneck(self):
-        # build a chain with an obvious bottleneck on normal graphs
+    def setUp(self) -> None:
         graph = FlowGraph([Vertex(n, label=chr(n + 97)) for n in range(4)])
         a, b, c, d, *_ = graph.nodes()
         graph.add_edge(a, b, 10)
         graph.add_edge(b, c, 2)
         graph.add_edge(c, d, 10)
+        graph.add_edge(d, a, 10)
 
-        aug_path = [graph.get_edge(a, b),
-                    graph.get_edge(b, c),
-                    graph.get_edge(c, d)]
+        self.graph = graph
+
+    def test_bottleneck(self):
+        # build a chain with an obvious bottleneck on normal graphs
+        graph = self.graph
+        a, b, c, d, *_ = graph.nodes()
+
+        aug_path = [
+            a, b, c, d
+        ]
 
         graph.to_dot()
 
         self.assertEqual(2, Simplify.bottleneck(graph, aug_path))
+
+    def test_nodes_to_path(self):
+        a, b, c, d, *_ = self.graph.nodes()
+        edges = Simplify.nodes_to_path(self.graph, [a, b, c, d])
+
+        self.assertEqual(edges, [FlowEdge(b, 10), FlowEdge(c, 2), FlowEdge(d, 10)])
+
+    def test_augment_flow(self):
+        a, b, c, d, *_ = self.graph.nodes()
+
+        Simplify.augment_flow(self.graph, [a, b, c, d], 2)
+        self.graph.to_dot()
+
+        # check that the edges and residual edges all now have flow 2
+        node_path = [a, b, c, d]
+        flow = 2
+        for _ in range(2):
+            # one for normal one for residual
+            for src, dest in zip(node_path, node_path[1:]):
+                with self.subTest(f'{src} -> {dest}'):
+                    self.assertEqual(flow, self.graph.get_edge(src, dest).flow)
+            # change path to be residual, flow changes accordingly
+            node_path.reverse()
+            flow *= -1
 
 
