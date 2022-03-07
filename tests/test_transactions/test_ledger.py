30b4537efd4a5188ca5a67a4d165b6bb90c46b92
Tom Cassar <cassar.thomas.e@gmail.com>
2022-03-07 20:13:10 +0000

    add tests for new transaction class

--- tests/test_transactions/test_ledger.py
+++ tests/test_transactions/test_ledger.py
@@ -1,8 +1,20 @@
+# coding=utf-8
 import unittest
+from src.transactions.transaction import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
+class TestLedger(unittest.TestCase):
+    def test_add(self):
+
+        with self.subTest("Add"):
+            ledger = Ledger()
+            self.assertFalse(not not ledger)
+            ledger.append(Transaction(0, 0, 0))
+            self.assertTrue(not not ledger)
+
+        with self.subTest("Catch non transaction"), self.assertRaises(LedgerBuildError):
+            ledger.append(6)  # type: ignore
+
+
+if __name__ == "__main__":
     unittest.main()
