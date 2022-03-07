d1538f70b886af476f45e9157057c82ffba54aa8
Tom Cassar <cassar.thomas.e@gmail.com>
2022-03-07 20:12:31 +0000

    added ledger class

--- src/transactions/transaction.py
+++ src/transactions/transaction.py
@@ -10,7 +10,11 @@ from src.crypto import keys
 
 import datetime
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+
+class LedgerBuildError(Exception):
+    """Error building ledger"""
 
 
 class Signable(ABC):
@@ -40,3 +44,27 @@ class Transaction(Signable):
 
     def verify_sig(self, public: keys.RSAPublicKey) -> None:
         """Raise verification error if invalid sig"""
+
+
+@dataclass
+class Ledger:
+    """Multiple transactions contained to one group; built from a stream of transaction objects"""
+    # ledger, big list of transactions;
+    # TODO: maybe make ledger generator
+    ledger: list[Transaction] = field(default_factory=lambda: [])
+
+    def __bool__(self):
+        """False if ledger empty"""
+        return not not self.ledger
+
+    def append(self, transaction: Transaction) -> list[Transaction]:
+        """Nice syntax for adding transactions to collection"""
+        print(type(transaction))
+
+        if type(transaction) is not Transaction:
+            raise LedgerBuildError(f'cannot append type {transaction} to ledger; must be transaction')
+        else:
+            self.ledger.append(transaction)
+
+        return self.ledger
+
