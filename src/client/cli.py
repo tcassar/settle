5b11eb37e830836e6791c6ac2512de58a5a55ead
Tom Cassar <cassar.thomas.e@gmail.com>
2022-03-11 17:00:29 +0000

    refactor

--- src/client/cli.py
+++ src/client/cli.py
@@ -8,29 +8,31 @@ from click.shell_completion import add_completion_class
 
 
 @click.group()
-def settle(): ...
+def settle():
+    ...
+
 
 # |--------|
 # |  USER  |
 # |--------|
 
 
-@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True)
-@click.option('--email', prompt=True)
+@click.option("--password", prompt=True, hide_input=True, confirmation_prompt=True)
+@click.option("--email", prompt=True)
 @settle.command()
 def login(email, password):
     """login to settle"""
-    password: bytes = hasher.Hasher(password.encode(encoding='utf8')).digest().h
+    password: bytes = hasher.Hasher(password.encode(encoding="utf8")).digest().h
 
     # TODO: offload both to server, wait for OKAY; if not okay, ask if account created
     # TODO: some control mechanism; make other functions only available given login
-    click.secho(f'login successful for {email}', fg='green')
+    click.secho(f"login successful for {email}", fg="green")
 
 
-@click.option('--pub_key', prompt='Path to RSA key')
-@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True)
-@click.option('--email', prompt=True)
-@click.option('--name', prompt='Full Name')
+@click.option("--pub_key", prompt="Path to RSA key")
+@click.option("--password", prompt=True, hide_input=True, confirmation_prompt=True)
+@click.option("--email", prompt=True)
+@click.option("--name", prompt="Full Name")
 @settle.command()
 def register(name, email, password, pub_key):
     """Register to settle, using email, passwd, and an RSA public key"""
@@ -43,20 +45,24 @@ def register(name, email, password, pub_key):
         ldr.parse()
         pub_key = keys.RSAPublicKey(ldr)
     except keys.RSAParserError as rsa_err:
-        click.secho(f'Failed to create account - issue with given RSA key;\n{rsa_err}', fg='red', bold=True)
+        click.secho(
+            f"Failed to create account - issue with given RSA key;\n{rsa_err}",
+            fg="red",
+            bold=True,
+        )
         create = False
 
-    password: bytes = hasher.Hasher(password.encode(encoding='utf8')).digest().h
+    password: bytes = hasher.Hasher(password.encode(encoding="utf8")).digest().h
 
-    click.echo(f'{name}, {email}, {password}, {pub_key}')
+    click.echo(f"{name}, {email}, {password}, {pub_key}")
 
     # TODO: Push info to server
     #   Check that email doesn't already exist
 
     if create:
-        click.secho(f'Account created successfully, using email {email}', fg='green')
+        click.secho(f"Account created successfully, using email {email}", fg="green")
     else:
-        click.secho(f'Account could not be created')
+        click.secho(f"Account could not be created")
 
 
 @settle.command()
@@ -64,17 +70,20 @@ def whoami():
     """gives your name, email, public key numbers in bytes"""
     ...
 
+
 # |----------------|
 # |  TRANSACTIONS  |
 # |----------------|
 
 
 @settle.command()
-def sign(): ...
+def sign():
+    ...
 
 
 @settle.command()
-def verify(): ...
+def verify():
+    ...
 
 
 # |----------|
@@ -83,15 +92,18 @@ def verify(): ...
 
 
 @settle.command()
-def join(): ...
+def join():
+    ...
 
 
 @settle.command()
 def leave():
     ...
 
+
 @settle.command()
-def simplify(): ...
+def simplify():
+    ...
 
 
 # |-----------|
@@ -100,9 +112,12 @@ def simplify(): ...
 
 # TODO: specify group or trn, show by id
 
+
 @settle.command()
-def new(): ...
+def new():
+    ...
 
 
 @settle.command()
-def show(): ...
+def show():
+    ...
